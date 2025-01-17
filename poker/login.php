35f1e12cf1ef08191a6a40b8d9e097500e3cdc86
yancell <35684687+yancell@users.noreply.github.com>
2025-01-17 20:55:25 +0700

    Delete poker directory

--- poker/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	require_once("config.php");
-    
-    echo "http://".$d."/auth/login_defor.php?userid=".$c."&sessid=".uniqid() . time()."&access_token=f2c80df303965957c33351982b173c04&sesskey=".uniqid() . time()."&is_kyc=false";
